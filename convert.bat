a7245928b1a9b25adae0de28f96fa5ad929ba5a7
Martin Reinhardt <martin.reinhardt.3@uni-leipzig.de>
2024-05-22 10:41:29 +0200

    added convert.bat for conversion from tif to netcdf

---
+++ convert.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Set the path to your tiff folder
+set "sourceDir=C:\Tiff-Directory"
+
+REM Set the path to the target directory where the NetCDF files will be saved
+set "targetDir=C:\C-Directory"
+
+REM Create the target directory if it doesn't exist
+if not exist "!targetDir!" mkdir "!targetDir!"
+
+REM Change the directory to the source directory
+cd /d "!sourceDir!"
+
+REM Loop through each TIFF file in the folder
+for %%f in (*.tif) do (
+    REM Define the output file name by replacing the extension and adding the target directory
+    set "outputName=!targetDir!\%%~nf.nc"
+    
+    REM gdalwarp to reproject and convert directly to NetCDF
+    REM gdalwarp -of NetCDF -co "FORMAT=NC4C" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" "%%f" "!outputName!"
+    REM gdalwarp -t_srs EPSG:3035 -of NetCDF -co "FORMAT=NC4C" -co "COMPRESS=DEFLATE" "%%f" "!outputName!"
+
+)
+
+echo Conversion completed.
+pause
