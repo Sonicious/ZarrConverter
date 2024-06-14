02b5ec5bf9e5576ccc015d14854355009fe62dfc
Martin Reinhardt <martin.reinhardt.3@uni-leipzig.de>
2024-06-14 14:50:34 +0200

    added script to convert TCIF data

---
+++ CreateTcsif.py
@@ -0,0 +1,79 @@
+import xarray as xr
+import numpy as np
+import rioxarray
+import zarr
+import os
+import glob
+import datetime
+import warnings
+
+def main():
+    
+    print("Converting TCSIF to Zarr format...")
+    
+    warnings.filterwarnings("ignore", category=UserWarning)
+    
+    from dask.distributed import Client
+    client = Client(n_workers=8, threads_per_worker=2, memory_limit='4GB')
+    # link to dashboard
+    print("Dashboard available under: " + str(client.dashboard_link))
+
+    # Set the directory where the data is stored
+    tiff_dir = "TCSIF_level3"
+    zarr_dir = "TCSIF_level3_2007_2021_1x360x720.zarr"
+    fill_value_new = np.nan
+
+    def CubeFile(file):
+        # extract date from filename
+        date = os.path.basename(file).split("_")[-1]
+        date = date.split(".")[0]
+        year = date[0:4]
+        month = date[4:6]
+        day = 15
+        cube = xr.open_dataarray(file,
+                                    engine="rasterio",
+                                    chunks={}
+        )
+        dt = np.datetime64(datetime.datetime(int(year), int(month), int(day)))
+        cube = cube.assign_coords({"time":dt})
+        cube = cube.expand_dims("time")
+        return cube
+    
+    print("Reading TIFF files...")
+
+    # Create the new dataset
+    files = glob.glob(tiff_dir + "/*.tif")
+    cube = xr.concat([CubeFile(file) for file in files], dim="time")
+    cube = cube.rename({"x":"lon", "y":"lat"})
+    ds = cube.to_dataset(dim="band")
+    ds = ds.rename_vars({1:"SIF"})
+
+    # set chunking
+    ds["SIF"] = ds["SIF"].chunk({"time":1, "lat":360, "lon":720})
+
+    sif_attrs = {
+        "long_name":"solar-induced chlorophyll fluorescence (SIF)",
+        "Unit":"W m-1 um-1 sr-1",
+        "_FillValue":fill_value_new,
+    }
+    ds["SIF"].attrs = sif_attrs
+    ds.attrs = {
+        "title":"TCSIF Level 3 SIF data (2007-2021)",
+        "history":"converted to zarr by Martin Reinhardt, RSC4Earth, University of Leipzig",
+        "source":"https://zenodo.org/records/8242928"
+    }
+
+    compressor = zarr.Blosc(cname="zstd", clevel=3, shuffle=2)
+    encoding = {vname: {
+        'compressor': compressor,
+        } for vname in ds.data_vars}
+    
+    print("Writing Zarr files...")
+
+    # ds.to_dataarray().to_zarr(zarr_dir, mode="w", consolidated=True, compute=True)
+    ds.to_zarr(zarr_dir, mode="w", consolidated=True, compute=True, encoding=encoding)
+
+    Client.close(client)
+
+if __name__ == '__main__':
+    main()
