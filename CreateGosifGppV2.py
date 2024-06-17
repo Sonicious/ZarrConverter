c8d46a210d5d2dd14410d5070202255d22796070
Martin Reinhardt <martin.reinhardt.3@uni-leipzig.de>
2024-06-17 13:37:18 +0200

    fixed band problems

--- CreateGosifGppV2.py
+++ CreateGosifGppV2.py
@@ -6,6 +6,8 @@ import os
 import glob
 import datetime
 import warnings
+from dask.distributed import Client
+from dask.diagnostics import ProgressBar
 
 def main():
     
@@ -13,13 +15,13 @@ def main():
     
     warnings.filterwarnings("ignore", category=UserWarning)
     
-    from dask.distributed import Client
-    client = Client(n_workers=2, threads_per_worker=2, memory_limit='6GB')
+    client = Client(n_workers=8, threads_per_worker=4, memory_limit='10GB')
+    # client = Client(n_workers=2, threads_per_worker=2, memory_limit='8GB')
     # link to dashboard
     print("Dashboard available under: " + str(client.dashboard_link))
 
     # Set the directory where the data is stored
-    tiff_dir = "GOSIF-GPP_v2"
+    tiff_dir = "GOSIF-GPP_v2/8day/Mean"
     zarr_dir = "GOSIF-GPP_v2_2000_2023_1x3600x7200.zarr"
     fill_value_old_1 = 65535 # fill value in the original data from README
     fill_value_old_2 = 65534 # fill value in the original data from README
@@ -48,11 +50,11 @@ def main():
     files = glob.glob(tiff_dir + "/*.tif")
     cube = xr.concat([CubeFile(file) for file in files], dim="time")
     cube = cube.rename({"x":"lon", "y":"lat"})
-    cube = cube.drop_vars("band")
-    ds = cube.to_dataset(name="GPP")
+    cube = cube.sel(band=1).drop_vars("band")
+    ds = cube.to_dataset(name="gpp")
 
     # set chunking
-    ds["GPP"] = ds["GPP"].chunk({"time":1, "lat":3600, "lon":7200})
+    ds["gpp"] = ds["gpp"].chunk({"time":1, "lat":3600, "lon":7200})
 
     gpp_attrs = {
         "long_name":"Gross Primary Production (GPP) from GOSIF",
@@ -60,7 +62,7 @@ def main():
         "_FillValue":fill_value_new,
         "Scale_Factor":0.001,
     }
-    ds["GPP"].attrs = gpp_attrs
+    ds["gpp"].attrs = gpp_attrs
     ds.attrs = {
         "title":"GOSIF GPP v2",
         "history":"converted to zarr by Martin Reinhardt, RSC4Earth, University of Leipzig",
@@ -74,7 +76,8 @@ def main():
     
     print("Writing Zarr files...")
 
-    ds.to_zarr(zarr_dir, mode="w", consolidated=True, compute=True, encoding=encoding)
+    with ProgressBar():
+        ds.to_zarr(zarr_dir, mode="w", consolidated=True, compute=True, encoding=encoding)
 
     Client.close(client)
 
